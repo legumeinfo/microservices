e0e2f5963d387910d1077b04a8f861dcf08ac64a
Alan Cleary <acleary@ncgr.org>
2021-03-09 12:59:10 -0700

    The Levenshtein metric was throwing an error due to deep recursion on larger inputs. To fix this the recursive algorithm was replaced with an iterative algorithm.

--- pairwise_macro_synteny_blocks/metrics.py
+++ pairwise_macro_synteny_blocks/metrics.py
@@ -1,27 +1,22 @@
-def _levenshteinRecurrence(a, i, b, j, t, mask):
-  # use memoized data if possible
-  if t[i][j] is None:
-    # base case: empty strings
-    if i == 0:
-      t[i][j] = j
-    elif j == 0:
-      t[i][j] = i
-    # test if last characters of the strings match
-    else:
-      cost = 0 if a[i-1] == b[j-1] and a[i-1] not in mask else 1
-      # return minimum of delete char from a, delete char from b, and delete
-      # char from both
-      t[i][j] = min(_levenshteinRecurrence(a, i-1, b, j, t, mask)+1,
-                    _levenshteinRecurrence(a, i, b, j-1, t, mask)+1,
-                    _levenshteinRecurrence(a, i-1, b, j-1, t, mask)+cost)
-  return t[i][j]
+from array import array
 
 
-def levenshtein(a, b, mask=set()):
-  i = len(a)
-  j = len(b)
-  t = [[None for n in range(j+1)] for m in range(i+1)]
-  return _levenshteinRecurrence(a, i, b, j, t, mask)
+def levenshtein(a, b):
+
+  if a == b: return 0
+  la, lb = len(a), len(b)
+  if la == 0: return lb
+  if lb == 0: return la
+  if lb > la: a, b, la, lb = b, a, lb, la
+
+  cost = array('i', range(lb + 1))
+  for i in range(1, la + 1):
+    cost[0] = i; ls = i-1; mn = ls
+    for j in range(1, lb + 1):
+      ls, act = cost[j], ls + int(a[i-1] != b[j-1])
+      cost[j] = min(ls+1, cost[j-1]+1, act)
+      if (ls < mn): mn = ls
+  return cost[lb]
 
 
 METRICS = {
