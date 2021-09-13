bab7f37f2af4cd9a8654fb36f86f1127428c555f
Alan Cleary <acleary@ncgr.org>
2021-09-13 13:04:17 -0600

    Updated the micro-synteny search service to compile its .proto files as part of its build process. This includes adding a service-specific .gitignore file that prevents the generated files from being added to the repository. The files that were previously generated by hand have been removed.

--- micro_synteny_search/micro_synteny_search/grpc_server.py
+++ micro_synteny_search/micro_synteny_search/grpc_server.py
@@ -2,10 +2,9 @@
 import grpc
 from grpc.experimental import aio
 # module
-from micro_synteny_search.services import microsyntenysearch_pb2
-from micro_synteny_search.services import microsyntenysearch_pb2_grpc
-from micro_synteny_search.structures import microtrack_pb2
-from micro_synteny_search.structures import track_pb2
+from micro_synteny_search.proto.microsyntenysearch_service.v1 import microsyntenysearch_pb2
+from micro_synteny_search.proto.microsyntenysearch_service.v1 import microsyntenysearch_pb2_grpc
+from micro_synteny_search.proto.track.v1 import track_pb2
 
 
 class MicroSyntenySearch(microsyntenysearch_pb2_grpc.MicroSyntenySearchServicer):
@@ -24,7 +23,7 @@ class MicroSyntenySearch(microsyntenysearch_pb2_grpc.MicroSyntenySearchServicer)
       await context.abort(grpc.StatusCode.INVALID_ARGUMENT, 'Required arguments are missing or have invalid values')
     tracks = await self.handler.process(query, matched, intermediate)
     track_messages = list(map(lambda t:
-      microtrack_pb2.MicroTrack(
+      track_pb2.MicroTrack(
         name=t['name'],
         track=track_pb2.Track(
           genus=t['genus'],
