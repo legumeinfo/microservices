73679584a71f1d761a325d78f0d42e722278ab81
Alan Cleary <acleary@ncgr.org>
2022-07-13 16:20:20 -0600

    Updated the genes service to use the redis package in place of the aioredis and redisearch packages.

--- genes/genes/request_handler.py
+++ genes/genes/request_handler.py
@@ -1,12 +1,30 @@
 # dependencies
-#from redisearch import Client
-from genes.aioredisearch import Client
+from redis.commands.search import AsyncSearch
+from redis.commands.search.document import Document
+from redis.commands.search._util import to_string
 
 
 class RequestHandler:
 
   def __init__(self, redis_connection):
     self.redis_connection = redis_connection
+
+  # decorate RediSearch get to return a Document instance for each id that
+  # exists in the index and None for those that don't, instead of raw contents
+  async def _get(self, index, *ids):
+    flat_fields = await index.get(*ids)
+    docs = []
+    for id, id_flat_fields in zip(ids, flat_fields):
+      if id_flat_fields is None:
+        docs.append(None)
+      else:
+        id_fields = dict(
+          dict(zip(map(to_string, id_flat_fields[::2]),
+                   map(to_string, id_flat_fields[1::2])))
+        )
+        doc = Document(id, payload=None, **id_fields)
+        docs.append(doc)
+    return docs
 
   def _geneDocToDict(self, gene_doc):
     return {
@@ -20,9 +38,9 @@ class RequestHandler:
 
   async def process(self, names):
     # connect to the index
-    gene_index = Client('geneIdx', conn=self.redis_connection)
+    gene_index = AsyncSearch(self.redis_connection, index_name='geneIdx')
     # get the genes from the index
     gene_ids = map(lambda name: f'gene:{name}', names)
-    docs = await gene_index.get(*gene_ids)
+    docs = await self._get(gene_index, *gene_ids)
     genes = list(map(self._geneDocToDict, filter(lambda d: d is not None, docs)))
     return genes
