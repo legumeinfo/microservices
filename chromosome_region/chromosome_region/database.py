026cb2ff3fd2c908bec9f3b0bed1675dff72e41b
Alan Cleary <acleary@ncgr.org>
2022-07-14 16:15:18 -0600

    Updated the chromosome_region service to use the redis package in place of the aioredis and redisearch packages.

--- chromosome_region/chromosome_region/database.py
+++ chromosome_region/chromosome_region/database.py
@@ -1,10 +1,10 @@
 # dependencies
-import aioredis
+import redis.asyncio as redis
 
 
 async def connectToRedis(host='localhost', port=6379, db=0, password=None):
   # connect to database
-  connection = await aioredis.Redis(host=host, port=port, db=db, password=password, decode_responses=True)
+  connection = await redis.Redis(host=host, port=port, db=db, password=password, decode_responses=True)
   # ping to force connection, preventing errors downstream
   await connection.ping()
   return connection
