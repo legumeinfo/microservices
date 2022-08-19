75b815a75164b5dcd8ec003c0de713a13d1a00cb
Alan Cleary <acleary@ncgr.org>
2022-08-19 16:24:53 -0600

    Fixed a bug in the gene search service that was preventing some genes from being included in the result set, such as those that exactly matched the search term.

--- gene_search/gene_search/request_handler.py
+++ gene_search/gene_search/request_handler.py
@@ -5,14 +5,22 @@ from redis.commands.search.query import Query
 
 class RequestHandler:
 
-  def __init__(self, redis_connection):
+  def __init__(self, redis_connection, breakpoint_characters=',.<>{}[]"\':;!@#$%^&*()-+=~'):
     self.redis_connection = redis_connection
+    self.breakpoint_characters = set(breakpoint_characters)
 
   async def process(self, name):
     # connect to the index
     gene_index = AsyncSearch(self.redis_connection, index_name='geneIdx')
+    # replace RediSearch breakpoint characters with spaces
+    cleaned_name = ''
+    for c in name:
+      if c in self.breakpoint_characters:
+        cleaned_name += ' '
+      else:
+        cleaned_name += c
     # search the gene index
-    query = Query(name)\
+    query = Query(cleaned_name)\
               .limit_fields('name')\
               .return_fields('name')
     result = await gene_index.search(query)
