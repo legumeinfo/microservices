4c0d7fa9c7844e7c725ef60bf1e74e4eadbf1acd
Alan Cleary <acleary@ncgr.org>
2022-08-26 12:39:11 -0600

    Implemented exception handling and graceful shutdowns in the pairwise macro-synteny blocks service.

--- pairwise_macro_synteny_blocks/pairwise_macro_synteny_blocks/grpc_server.py
+++ pairwise_macro_synteny_blocks/pairwise_macro_synteny_blocks/grpc_server.py
@@ -12,7 +12,14 @@ class PairwiseMacroSyntenyBlocks(pairwisemacrosyntenyblocks_pb2_grpc.PairwiseMac
   def __init__(self, handler):
     self.handler = handler
 
-  async def Compute(self, request, context):
+  # create a context done callback that raises the given exception
+  def _exceptionCallbackFactory(self, exception):
+    def exceptionCallback(call):
+      raise exception
+    return exceptionCallback
+
+  # the method that actually handles requests
+  async def _compute(self, request, context):
     # required parameters
     chromosome = request.chromosome
     target = request.target
@@ -44,6 +51,22 @@ class PairwiseMacroSyntenyBlocks(pairwisemacrosyntenyblocks_pb2_grpc.PairwiseMac
       ), blocks))
     return pairwisemacrosyntenyblocks_pb2.PairwiseMacroSyntenyBlocksComputeReply(blocks=block_messages)
 
+  # implements the service's API
+  async def Compute(self, request, context):
+    # subvert the gRPC exception handler via a try/except block
+    try:
+      return await self._compute(request, context)
+    # let errors we raised go by
+    except aio.AbortError as e:
+      raise e
+    # raise an internal error to prevent non-gRPC info from being sent to users
+    except Exception as e:
+      # raise the exception after aborting so it gets logged
+      # NOTE: gRPC docs says abort should raise an error but it doesn't...
+      context.add_done_callback(self._exceptionCallbackFactory(e))
+      # return a gRPC INTERNAL error
+      await context.abort(grpc.StatusCode.INTERNAL, 'Internal server error')
+
 
 async def run_grpc_server(host, port, handler):
   server = aio.server()
@@ -53,3 +76,4 @@ async def run_grpc_server(host, port, handler):
     .add_PairwiseMacroSyntenyBlocksServicer_to_server(servicer, server)
   await server.start()
   await server.wait_for_termination()
+  # TODO: what about teardown? server.stop(None)
