cb14dc11453b5fcb3f3c9369bfa134e3e3d4d156
Alan Cleary <acleary@ncgr.org>
2022-08-26 12:37:41 -0600

    Implemented exception handling and graceful shutdowns in the gene search service.

--- gene_search/gene_search/grpc_server.py
+++ gene_search/gene_search/grpc_server.py
@@ -1,4 +1,5 @@
 # dependencies
+import grpc
 from grpc.experimental import aio
 # module
 from gene_search.proto.genesearch_service.v1 import genesearch_pb2
@@ -10,9 +11,32 @@ class GeneSearch(genesearch_pb2_grpc.GeneSearchServicer):
   def __init__(self, handler):
     self.handler = handler
 
-  async def Search(self, request, context):
+  # create a context done callback that raises the given exception
+  def _exceptionCallbackFactory(self, exception):
+    def exceptionCallback(call):
+      raise exception
+    return exceptionCallback
+
+  # the method that actually handles requests
+  async def _search(self, request, context):
     genes = await self.handler.process(request.query)
     return genesearch_pb2.GeneSearchReply(genes=genes)
+
+  # implements the service's API
+  async def Search(self, request, context):
+    # subvert the gRPC exception handler via a try/except block
+    try:
+      return await self._search(request, context)
+    # let errors we raised go by
+    except aio.AbortError as e:
+      raise e
+    # raise an internal error to prevent non-gRPC info from being sent to users
+    except Exception as e:
+      # raise the exception after aborting so it gets logged
+      # NOTE: gRPC docs says abort should raise an error but it doesn't...
+      context.add_done_callback(self._exceptionCallbackFactory(e))
+      # return a gRPC INTERNAL error
+      await context.abort(grpc.StatusCode.INTERNAL, 'Internal server error')
 
 
 async def run_grpc_server(host, port, handler):
@@ -22,3 +46,4 @@ async def run_grpc_server(host, port, handler):
   genesearch_pb2_grpc.add_GeneSearchServicer_to_server(servicer, server)
   await server.start()
   await server.wait_for_termination()
+  # TODO: what about teardown? server.stop(None)
