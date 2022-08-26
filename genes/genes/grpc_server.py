10c5183cdcd733c274ef5a60fe1776fcf2aa00f3
Alan Cleary <acleary@ncgr.org>
2022-08-26 12:37:57 -0600

    Implemented exception handling and graceful shutdowns in the genes service.

--- genes/genes/grpc_server.py
+++ genes/genes/grpc_server.py
@@ -1,4 +1,5 @@
 # dependencies
+import grpc
 from grpc.experimental import aio
 # module
 from genes.proto.genes_service.v1 import genes_pb2
@@ -11,7 +12,14 @@ class Genes(genes_pb2_grpc.GenesServicer):
   def __init__(self, handler):
     self.handler = handler
 
-  async def Get(self, request, context):
+  # create a context done callback that raises the given exception
+  def _exceptionCallbackFactory(self, exception):
+    def exceptionCallback(call):
+      raise exception
+    return exceptionCallback
+
+  # the method that actually handles requests
+  async def _get(self, request, context):
     genes = await self.handler.process(request.names)
     gene_messages = list(map(lambda g:
       gene_pb2.Gene(
@@ -24,6 +32,22 @@ class Genes(genes_pb2_grpc.GenesServicer):
       ), genes))
     return genes_pb2.GenesGetReply(genes=gene_messages)
 
+  # implements the service's API
+  async def Get(self, request, context):
+    # subvert the gRPC exception handler via a try/except block
+    try:
+      return await self._get(request, context)
+    # let errors we raised go by
+    except aio.AbortError as e:
+      raise e
+    # raise an internal error to prevent non-gRPC info from being sent to users
+    except Exception as e:
+      # raise the exception after aborting so it gets logged
+      # NOTE: gRPC docs says abort should raise an error but it doesn't...
+      context.add_done_callback(self._exceptionCallbackFactory(e))
+      # return a gRPC INTERNAL error
+      await context.abort(grpc.StatusCode.INTERNAL, 'Internal server error')
+
 
 async def run_grpc_server(host, port, handler):
   server = aio.server()
@@ -32,3 +56,4 @@ async def run_grpc_server(host, port, handler):
   genes_pb2_grpc.add_GenesServicer_to_server(servicer, server)
   await server.start()
   await server.wait_for_termination()
+  # TODO: what about teardown? server.stop(None)
