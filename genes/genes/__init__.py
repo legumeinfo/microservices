d118c9e2ac8abd9ab76e37d7ea4d2e120b25eb67
Alan Cleary <acleary@ncgr.org>
2022-10-13 12:32:25 -0600

    Bumped genes version to 1.1.1.

--- genes/genes/__init__.py
+++ genes/genes/__init__.py
@@ -4,7 +4,7 @@ def int_or_str(value):
   except ValueError:
     return value
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 VERSION = tuple(map(int_or_str, __version__.split('.')))
 
 __schema_version__ = '1.1.0'
