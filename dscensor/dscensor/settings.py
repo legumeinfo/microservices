fd1ba35f993953e6a2c3507ae9f47ed13c724ec9
Connor Cameron <ctc@ncgr.org>
2023-11-07 16:18:35 -0500

    Working compose. It is required that one uses lis-autocontent to setup a "./autocontent" directory for the nodes. see https://github.com/legumeinfo/DSCensor/tree/openapi proper for more information. #611

--- dscensor/dscensor/settings.py
+++ dscensor/dscensor/settings.py
@@ -5,4 +5,4 @@ from rororo import BaseSettings
 @environ.config(prefix=None, frozen=True)
 class Settings(BaseSettings):
     dscensor_app_key: str = environ.var(name="DSCENSOR_APP_KEY", default="digraph")
-    input_nodes: str = environ.var(name="DSCENSOR_APP_KEY", default="./autocontent")
+    input_nodes: str = environ.var(name="DSCENSOR_INPUT_NODES", default="./autocontent")
