00c3c12daa735b055f892d6a73d4700917582757
Connor Cameron <ctc@ncgr.org>
2024-11-13 13:32:57 -0700

    working fasta_api, with proper error handling. #629

--- fasta_api/fasta_api/http_server.py
+++ fasta_api/fasta_api/http_server.py
@@ -2,14 +2,17 @@
 import aiohttp_cors
 from aiohttp import web
 
+from fastapi import HTTPException, status
+
+
 async def http_index(request):
     return web.Response(text="Index")
 
+
 async def http_help(request):
-    test = request.match_info.get('test')
-    print(test)
     resources = [str(resource) for resource in request.app.router.resources()]
     return web.json_response(resources)
+
 
 async def http_fasta_range(request):
     seqid = request.match_info.get('seqid')
@@ -18,31 +21,46 @@ async def http_fasta_range(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     range = handler.fasta_range(url, seqid, start, end)
+    if "error" in range:
+        return web.json_response(range, status=range["status"])
     return web.json_response(range)
+
 
 async def http_fasta_references(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     references = handler.fasta_references(url)
+    if "error" in references:
+        return web.json_response(references, status=references["status"])
     return web.json_response(references)
+
 
 async def http_fasta_lengths(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     lengths = handler.fasta_lengths(url)
+    if "error" in lengths:
+        return web.json_response(lengths, status=lengths["status"])
     return web.json_response(lengths)
+
 
 async def http_fasta_nreferences(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     nreferences = handler.fasta_nreferences(url)
+    if "error" in nreferences:
+        return web.json_response(nreferences, status=nreferences["status"])
     return web.json_response(nreferences)
+
 
 async def http_gff_references(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     references = handler.gff_references(url)
+    if "error" in references:
+        return web.json_response(references, status=references["status"])
     return web.json_response(references)
+
 
 async def http_gff_features(request):
     seqid = request.match_info.get('seqid')
@@ -51,7 +69,10 @@ async def http_gff_features(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     features = handler.gff_features(url, seqid, start, end)
+    if "error" in features:
+        return web.json_response(features, status=features["status"])
     return web.json_response(features)
+
 
 async def http_bed_features(request):
     seqid = request.match_info.get('seqid')
@@ -60,13 +81,19 @@ async def http_bed_features(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     features = handler.bed_features(url, seqid, start, end)
+    if "error" in features:
+        return web.json_response(features, status=features["status"])
     return web.json_response(features)
+
 
 async def http_vcf_contigs(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     contigs = handler.vcf_contigs(url)
+    if "error" in contigs:
+        return web.json_response(contigs, status=contigs["status"])
     return web.json_response(contigs)
+
 
 async def http_vcf_features(request):
     seqid = request.match_info.get('seqid')
@@ -75,49 +102,73 @@ async def http_vcf_features(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     features = handler.vcf_features(url, seqid, start, end)
+    if "error" in features:
+        return web.json_response(features, status=features["status"])
     return web.json_response(features)
+
 
 async def http_alignment_references(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     references = handler.alignment_references(url)
+    if "error" in references:
+        return web.json_response(references, status=references["status"])
     return web.json_response(references)
+
 
 async def http_alignment_unmapped(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     unmapped = handler.alignment_unmapped(url)
+    if "error" in unmapped:
+        return web.json_response(unmapped, status=unmapped["status"])
     return web.json_response(unmapped)
+
 
 async def http_alignment_nreferences(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     nreferences = handler.alignment_nreferences(url)
+    if "error" in nreferences:
+        return web.json_response(nreferences, status=nreferences["status"])
     return web.json_response(nreferences)
+
 
 async def http_alignment_nocoordinate(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     nocoordinate = handler.alignment_nocoordinate(url)
+    if "error" in nocoordinate:
+        return web.json_response(nocoordinate, status=nocoordinate["status"])
     return web.json_response(nocoordinate)
+
 
 async def http_alignment_mapped(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     mapped = handler.alignment_mapped(url)
+    if "error" in mapped:
+        return web.json_response(mapped, status=mapped["status"])
     return web.json_response(mapped)
+
 
 async def http_alignment_lengths(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     lengths = handler.alignment_lengths(url)
+    if "error" in lengths:
+        return web.json_response(lengths, status=lengths["status"])
     return web.json_response(lengths)
+
 
 async def http_alignment_index_statistics(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     statistics = handler.alignment_index_statistics(url)
+    if "error" in statistics:
+        return web.json_response(statistics, status=statistics["status"])
     return web.json_response(statistics)
+
 
 async def http_alignment_count(request):
     contig = request.match_info.get('contig')
@@ -126,7 +177,10 @@ async def http_alignment_count(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     count = handler.alignment_count(url, contig, start, stop)
+    if "error" in count:
+        return web.json_response(count, status=count["status"])
     return web.json_response(count)
+
 
 async def http_alignment_count_coverage(request):
     contig = request.match_info.get('contig')
@@ -135,7 +189,10 @@ async def http_alignment_count_coverage(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     coverage = handler.alignment_count_coverage(url, contig, start, stop)
+    if "error" in coverage:
+        return web.json_response(coverage, status=coverage["status"])
     return web.json_response(coverage)
+
 
 async def http_alignment_fetch(request):
     contig = request.match_info.get('contig')
@@ -144,14 +201,20 @@ async def http_alignment_fetch(request):
     url = request.match_info.get('url')
     handler = request.app["handler"]
     fetch = handler.alignment_fetch(url, contig, start, stop)
+    if "error" in fetch:
+        return web.json_response(fetch, status=fetch["status"])
     return web.json_response(fetch)
+
 
 async def http_alignment_reference_lengths(request):
     reference = request.match_info.get('reference')
     url = request.match_info.get('url')
     handler = request.app["handler"]
     lengths = handler.alignment_reference_lengths(url, reference)
+    if "error" in lengths:
+        return web.json_response(lengths, status=lengths["status"])
     return web.json_response(lengths)
+
 
 def run_http_server(host, port, handler):
     # make the app
@@ -170,7 +233,7 @@ def run_http_server(host, port, handler):
     )
     route = app.router.add_get("/", http_index)
     cors.add(route)
-    route = app.router.add_get("/help/{test}", http_help)
+    route = app.router.add_get("/help", http_help)
     cors.add(route)
     route = app.router.add_get("/fasta/fetch/{seqid}:{start}-{end}/{url}", http_fasta_range)
     cors.add(route)
--- fasta_api/fasta_api/request_handler.py
+++ fasta_api/fasta_api/request_handler.py
@@ -8,6 +8,7 @@ import itertools
 
 ALLOWED_URLS = os.environ.get("ALLOWED_URLS", "").split(",")
 
+
 class RequestHandler:
     def __init__(self):
         pass
@@ -15,57 +16,69 @@ class RequestHandler:
     def check_url(self, url):
         url = urllib.parse.unquote(url)
         if not any(url.startswith(allowed_url) for allowed_url in ALLOWED_URLS):
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="url not allowed")
+            return {"error": "url not allowed", "status": status.HTTP_403_FORBIDDEN}
         return url
 
     def send_400_resp(self, msg):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+        return {"error": msg, "status": status.HTTP_400_BAD_REQUEST}
 
     def index(self):
         return "fasta-api"
 
     def fasta_range(self, url: str, seqid: str, start: int = None, end: int = None):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             seq = pysam.FastaFile(url).fetch(reference=seqid, start=start, end=end)
             return {"sequence": seq}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
         except KeyError as e:
-            self.send_400_resp(f"Unable to find feature: {e}")
+            return self.send_400_resp(f"Unable to find feature: {e}")
         except ValueError as e:
-            self.send_400_resp(f"Unable to interpret coordinates: {e}")
+            return self.send_400_resp(f"Unable to interpret coordinates: {e}")
 
     def fasta_references(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"references": pysam.FastaFile(url).references}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def fasta_lengths(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"lengths": pysam.FastaFile(url).lengths}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def fasta_nreferences(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"nreferences": pysam.FastaFile(url).nreferences}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def gff_references(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"contigs": pysam.TabixFile(url).contigs}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def gff_features(self, url: str, seqid: str, start: int = None, end: int = None):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return [{"contig": feature.contig,
                      "feature": feature.feature,
@@ -79,39 +92,45 @@ class RequestHandler:
                     for feature
                     in pysam.TabixFile(url).fetch(seqid, start, end, parser=pysam.asGFF3())]
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
         except KeyError as e:
-            self.send_400_resp(f"Unable to find feature: {e}")
+            return self.send_400_resp(f"Unable to find feature: {e}")
         except ValueError as e:
-            self.send_400_resp(f"Unable to find index: {e}")
+            return self.send_400_resp(f"Unable to find index: {e}")
 
     def bed_features(self, url: str, seqid: str, start: int = None, end: int = None):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             bedcols = (
-            'contig', 'start', 'end', 'name', 'score', 'strand', 'thickStart', 'thickEnd', 'itemRGB', 'blockCount',
-            'blockSizes', 'blockStarts')
+                'contig', 'start', 'end', 'name', 'score', 'strand', 'thickStart', 'thickEnd', 'itemRGB', 'blockCount',
+                'blockSizes', 'blockStarts')
             return [dict(itertools.starmap(lambda k, v: (
-            k, int(v) if k in ['start', 'end', 'thickStart', 'thickEnd'] else float(v) if k == 'score' else v),
+                k, int(v) if k in ['start', 'end', 'thickStart', 'thickEnd'] else float(v) if k == 'score' else v),
                                            zip(bedcols, feature)))
                     for feature
                     in pysam.TabixFile(url).fetch(seqid, start, end, parser=pysam.asBed())]
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
         except KeyError as e:
-            self.send_400_resp(f"Unable to find feature: {e}")
+            return self.send_400_resp(f"Unable to find feature: {e}")
         except ValueError as e:
-            self.send_400_resp(f"Unable to find index: {e}")
+            return self.send_400_resp(f"Unable to find index: {e}")
 
     def vcf_contigs(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"contigs": list(pysam.VariantFile(url).header.contigs)}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def vcf_features(self, url: str, seqid: str, start: int = None, end: int = None):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return [{"chrom": feature.chrom,
                      "pos": feature.pos,
@@ -127,69 +146,87 @@ class RequestHandler:
                     for feature
                     in pysam.VariantFile(url).fetch(seqid, start, end)]
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
         except KeyError as e:
-            self.send_400_resp(f"Unable to find feature: {e}")
+            return self.send_400_resp(f"Unable to find feature: {e}")
 
     def alignment_references(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"references": pysam.AlignmentFile(url).references}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def alignment_unmapped(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"unmapped": pysam.AlignmentFile(url).unmapped}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def alignment_nreferences(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"nreferences": pysam.AlignmentFile(url).nreferences}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def alignment_nocoordinate(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"nocoordinate": pysam.AlignmentFile(url).nocoordinate}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def alignment_mapped(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"mapped": pysam.AlignmentFile(url).mapped}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def alignment_lengths(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"lengths": pysam.AlignmentFile(url).lengths}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def alignment_index_statistics(self, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"index_statistics": pysam.AlignmentFile(url).get_index_statistics()}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def alignment_count(self, url: str, contig: str, start: int, stop: int):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             count = pysam.AlignmentFile(url).count(contig, start, stop)
             return {"count": count}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
 
     def alignment_count_coverage(self, url: str, contig: str, start: int, stop: int):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             count_coverage = pysam.AlignmentFile(url).count_coverage(contig, start, stop)
             return [{"A": json.dumps([x for x in count_coverage[0]]),
@@ -198,25 +235,33 @@ class RequestHandler:
                      "D": json.dumps([x for x in count_coverage[3]])
                      }]
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
         except KeyError as e:
-            self.send_400_resp(f"Unable to find feature: {e}")
+            return self.send_400_resp(f"Unable to find feature: {e}")
 
     def alignment_fetch(self, url: str, contig: str, start: int = None, stop: int = None):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return [feature.to_dict()
                     for feature
                     in pysam.AlignmentFile(url).fetch(contig=contig, start=start, stop=stop)]
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}")
         except KeyError as e:
-            self.send_400_resp(f"Unable to find feature: {e}")
+            return self.send_400_resp(f"Unable to find feature: {e}")
 
     def alignment_reference_lengths(self, reference: str, url: str):
         url = self.check_url(url)
+        if isinstance(url, dict):
+            return url
         try:
             return {"length": pysam.AlignmentFile(url).get_reference_length(reference)}
         except OSError as e:
-            self.send_400_resp(f"Unable to open file: {e}")
+            return self.send_400_resp(f"Unable to open file: {e}") 
 
+
+            return {"unmapped": pysam.AlignmentFile(url).unmapped}
+        except OSError as e:
+            return self.send_400_resp(f"Unable to open file: {e}")
