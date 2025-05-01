1ad6fbbea1b5afff1bec39ae43ad6cdcc52ca5f7
Alan Cleary <acleary@ncgr.org>
2025-05-01 16:47:22 -0600

    Bumped pairwise macro-synteny blocks version to 1.3.4

--- pairwise_macro_synteny_blocks/pairwise_macro_synteny_blocks/__init__.py
+++ pairwise_macro_synteny_blocks/pairwise_macro_synteny_blocks/__init__.py
@@ -5,7 +5,7 @@ def int_or_str(value):
         return value
 
 
-__version__ = "1.3.3"
+__version__ = "1.3.4"
 VERSION = tuple(map(int_or_str, __version__.split(".")))
 
 __schema_version__ = "1.1.0"
